e1cd26728aebdac92b0e56b0e28b8f8f93c82b08
zachary-vincent <45057145+ZachieV@users.noreply.github.com>
2019-06-14 18:24:14 -0700

    added styling and functionality
    and axios stuff

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
-  );
+const StyledSmurf = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: 2rem auto;
+	width: 40%;
+	font-size: 2rem;
+	background-color: steelblue;
+	border-radius: 5px;
+	color: #fff;
+`;
+
+const Smurf = (props) => {
+	return (
+		<StyledSmurf>
+			<h3>{props.name}</h3>
+			<strong>{props.height} tall</strong>
+			<p>{props.age} smurf years old</p>
+		</StyledSmurf>
+	);
 };
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+	name: '',
+	height: '',
+	age: ''
 };
 
 export default Smurf;
-
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import Smurf from './Smurf';
 
+const StyledSmurfsContainer = styled.div`
+	h1 {
+		font-size: 3rem;
+	}
+`;
+
 class Smurfs extends Component {
-  render() {
-    return (
-      <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<StyledSmurfsContainer>
+				<ul>
+					{this.props.smurfs.map((smurf) => {
+						return (
+							<Smurf
+								name={smurf.name}
+								id={smurf.id}
+								age={smurf.age}
+								height={smurf.height}
+								key={smurf.id}
+							/>
+						);
+					})}
+				</ul>
+			</StyledSmurfsContainer>
+		);
+	}
 }
 
 Smurf.defaultProps = {
- smurfs: [],
+	smurfs: []
 };
 
 export default Smurfs;
